3f0dc9c753e099665b96f8d85c2476df9737058e
Dimitrios Tsimpoukis <dimitrios.tsimpoukis@nl.abnamro.com>
2020-10-19 09:05:30 +0200

    edits

--- remote_setup.sh
+++ remote_setup.sh
@@ -1,9 +1,11 @@
 cp .vimrc ~/.vimrc
 cp .ideavimrc ~/.ideavimrc
 cp .vimrc.keymap ~/.vimrc.keymap
+mkdir ~/.vim/swp
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -k -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 vim +PluginInstall +qall
