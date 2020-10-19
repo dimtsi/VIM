c5bd91630c158b7ca533c408af1200555af524ae
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2020-10-19 08:35:47 +0200

    keymap

--- remote_setup.sh
+++ remote_setup.sh
@@ -1,5 +1,6 @@
 cp .vimrc ~/.vimrc
 cp .ideavimrc ~/.ideavimrc
+cp .vimrc.keymap ~/.vimrc.keymap
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
